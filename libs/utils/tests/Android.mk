7677239b54269b2dc2e195516648ec68697f3a1c
Kenny Root <kroot@google.com>
2012-10-02 09:09:10 -0700

    Update tests for new build target
    Change-Id: Ia1740d1b2c0b611c4559c5c846b12fb5c3e81b07

--- libs/utils/tests/Android.mk
+++ libs/utils/tests/Android.mk
@@ -4,24 +4,24 @@ include $(CLEAR_VARS)
 
 # Build the unit tests.
 test_src_files := \
-	BasicHashtable_test.cpp \
-	BlobCache_test.cpp \
-	Looper_test.cpp \
-	String8_test.cpp \
-	Unicode_test.cpp \
-	Vector_test.cpp \
-	ZipFileRO_test.cpp
+    BasicHashtable_test.cpp \
+    BlobCache_test.cpp \
+    Looper_test.cpp \
+    String8_test.cpp \
+    Unicode_test.cpp \
+    Vector_test.cpp \
+    ZipFileRO_test.cpp
 
 shared_libraries := \
-	libz \
-	liblog \
-	libcutils \
-	libutils \
-	libstlport
+    libz \
+    liblog \
+    libcutils \
+    libutils \
+    libstlport
 
 static_libraries := \
-	libgtest \
-	libgtest_main
+    libgtest \
+    libgtest_main
 
 c_includes := \
     external/zlib \
@@ -31,8 +31,6 @@ c_includes := \
     external/gtest/include \
     external/stlport/stlport
 
-module_tags := eng tests
-
 $(foreach file,$(test_src_files), \
     $(eval include $(CLEAR_VARS)) \
     $(eval LOCAL_SHARED_LIBRARIES := $(shared_libraries)) \
@@ -40,6 +38,5 @@ $(foreach file,$(test_src_files), \
     $(eval LOCAL_C_INCLUDES := $(c_includes)) \
     $(eval LOCAL_SRC_FILES := $(file)) \
     $(eval LOCAL_MODULE := $(notdir $(file:%.cpp=%))) \
-    $(eval LOCAL_MODULE_TAGS := $(module_tags)) \
-    $(eval include $(BUILD_EXECUTABLE)) \
+    $(eval include $(BUILD_NATIVE_TEST)) \
 )

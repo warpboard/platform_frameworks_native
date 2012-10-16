8156a8e7ca0822ae343c1b7bdf6a74ffe8026c1e
Kenny Root <kroot@google.com>
2012-10-16 11:34:02 -0700

    Add TEMP_FAILURE_RETRY to ZipUtils
    Change-Id: I275c415f14eeffaf9a58d45f3ea014d766441ec3

--- libs/utils/ZipFileRO.cpp
+++ libs/utils/ZipFileRO.cpp
@@ -20,6 +20,7 @@
 #define LOG_TAG "zipro"
 //#define LOG_NDEBUG 0
 #include <utils/Log.h>
+#include <utils/Compat.h>
 #include <utils/ZipFileRO.h>
 #include <utils/misc.h>
 #include <utils/threads.h>
@@ -46,21 +47,6 @@
  */
 #ifndef O_BINARY
 #  define O_BINARY  0
-#endif
-
-/*
- * TEMP_FAILURE_RETRY is defined by some, but not all, versions of
- * <unistd.h>. (Alas, it is not as standard as we'd hoped!) So, if it's
- * not already defined, then define it here.
- */
-#ifndef TEMP_FAILURE_RETRY
-/* Used to retry syscalls that can return EINTR. */
-#define TEMP_FAILURE_RETRY(exp) ({         \
-    typeof (exp) _rc;                      \
-    do {                                   \
-        _rc = (exp);                       \
-    } while (_rc == -1 && errno == EINTR); \
-    _rc; })
 #endif
 
 using namespace android;

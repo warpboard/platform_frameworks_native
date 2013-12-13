8e77fd127e70704f9e9c8285fb26e2995faee412
Brian Carlstrom <bdc@google.com>
2013-12-12 23:13:18 -0800

    Track Looper decoupling from ALooper
    Change-Id: I612f0de7002e24948a4cbbea7b416b4fbb3929aa

--- services/surfaceflinger/MessageQueue.cpp
+++ services/surfaceflinger/MessageQueue.cpp
@@ -105,7 +105,7 @@ void MessageQueue::setEventThread(const sp<EventThread>& eventThread)
     mEventThread = eventThread;
     mEvents = eventThread->createEventConnection();
     mEventTube = mEvents->getDataChannel();
-    mLooper->addFd(mEventTube->getFd(), 0, ALOOPER_EVENT_INPUT,
+    mLooper->addFd(mEventTube->getFd(), 0, LOOPER_EVENT_INPUT,
             MessageQueue::cb_eventReceiver, this);
 }
 
@@ -114,12 +114,12 @@ void MessageQueue::waitMessage() {
         IPCThreadState::self()->flushCommands();
         int32_t ret = mLooper->pollOnce(-1);
         switch (ret) {
-            case ALOOPER_POLL_WAKE:
-            case ALOOPER_POLL_CALLBACK:
+            case LOOPER_POLL_WAKE:
+            case LOOPER_POLL_CALLBACK:
                 continue;
-            case ALOOPER_POLL_ERROR:
-                ALOGE("ALOOPER_POLL_ERROR");
-            case ALOOPER_POLL_TIMEOUT:
+            case LOOPER_POLL_ERROR:
+                ALOGE("LOOPER_POLL_ERROR");
+            case LOOPER_POLL_TIMEOUT:
                 // timeout (should not happen)
                 continue;
             default:

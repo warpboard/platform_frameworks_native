75ababd315a18d15fab1363bd55f121222345c1c
Arve Hjønnevåg <arve@android.com>
2014-01-28 19:57:02 -0800

    Add BINDER_IPC_32BIT to CFLAGS unless TARGET_USES_64BIT_BINDER is true
    Change-Id: I96c643123b0314c361b7f48a18d5c22c660d4ff5

--- cmds/servicemanager/Android.mk
+++ cmds/servicemanager/Android.mk
@@ -5,6 +5,10 @@ LOCAL_PATH:= $(call my-dir)
 #LOCAL_MODULE := bctest
 #include $(BUILD_EXECUTABLE)
 
+ifneq ($(TARGET_USES_64BIT_BINDER),true)
+svc_c_flags += -DBINDER_IPC_32BIT=1
+endif
+
 include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_SRC_FILES := service_manager.c binder.c
